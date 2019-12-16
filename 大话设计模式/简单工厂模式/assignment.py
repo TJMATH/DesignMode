00aa7d409f75bd048a947a1e1ce1e218b6d615af
kyang <tjmath_yk@163.com>
2019-12-17 01:04:58 +0800

    [add] 大话设计模式 简单工厂模式作业 done

--- 大话设计模式/简单工厂模式/assignment.py
+++ 大话设计模式/简单工厂模式/assignment.py
@@ -5,12 +5,26 @@ task: 简单工厂模式实现四则运算计算器
 
 class Operation(object):
     def __init__(self):
-        self.numA = None
-        self.numB = None
+        self._numA = None
+        self._numB = None
 
-    @set
+    @property
     def numA(self):
-        
+        return self._numA
+
+    @numA.setter
+    def numA(self, num):
+        assert isinstance(num, float) or isinstance(num, int), '必须输入数字'
+        self._numA = num
+
+    @property
+    def numB(self):
+        return self._numB
+
+    @numB.setter
+    def numB(self, num):
+        assert isinstance(num, float) or isinstance(num, int), '必须输入数字'
+        self._numB = num
 
     def cal(self):
         NotImplemented()
@@ -20,14 +34,41 @@ class AddOpertion(Operation):
     def cal(self):
         return self.numA + self.numB
 
-class Opertion(Operation):
+class SubtrOpertion(Operation):
     def cal(self):
-        return self.numA + self.numB
+        return self.numA - self.numB
 
-class AddOpertion(Operation):
+class MultiOpertion(Operation):
     def cal(self):
-        return self.numA + self.numB
+        return self.numA * self.numB
 
-class AddOpertion(Operation):
+class DivOpertion(Operation):
     def cal(self):
-        return self.numA + self.numB
+        assert self.numB != 0, '除0错误'
+        return self.numA / self.numB
+
+class OperationFactory(object):
+    def __init__(self):
+        self.opertions = {
+            '+': AddOpertion,
+            '-': SubtrOpertion,
+            '*': MultiOpertion,
+            '/': DivOpertion
+        }
+
+    def get_operation(self, operation):
+        return self.opertions[operation]()
+
+
+def test(num1, num2):
+    operFactory = OperationFactory()
+    print('============================================')
+    for op in ['+', '-', '*', '/']:
+        oper = operFactory.get_operation(op)
+        oper.numA = num1
+        oper.numB = num2
+        print('{} {} {} = {}'.format(num1, op, num2, oper.cal()))
+
+if __name__ == '__main__':
+    test(1, 2)
+    test(3.2, 0)
